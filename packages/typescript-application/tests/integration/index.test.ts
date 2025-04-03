687d167c90451222aad870b5eb6bca369d881233
Jonathan Morley <jmorley@cvent.com>
2025-04-03 18:00:07 -0400

    add its

---
+++ packages/typescript-application/tests/integration/index.test.ts
@@ -0,0 +1,7 @@
+import { it } from "vitest";
+import { setTimeout } from "node:timers/promises";
+
+it("should wait for 10 seconds", { timeout: 15_000 }, async ({ expect }) => {
+  await setTimeout(10_000);
+  expect(true).toBe(true);
+});
--- packages/typescript-library/tests/unit/index.test.ts
+++ packages/typescript-library/tests/unit/index.test.ts
@@ -1,7 +1,7 @@
-import { expect, it } from "vitest";
+import { it } from "vitest";
 import { hello } from "../../src/index.js";
 
-it("should return hello world", () => {
+it("should return hello world", ({ expect }) => {
   const result = hello("World");
   expect(result).toBe("Hello World!");
 });
